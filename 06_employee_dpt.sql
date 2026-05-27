1f9f124d391cb54df575acdd8f48d1ea1c5d1c5a
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2026-05-28 02:08:03 +0530

    add query for employee no 900

---
+++ 06_employee_dpt.sql
@@ -0,0 +1,6 @@
+-- Query to display Employee Name and Department Number for the Employee No = 900
+
+SELECT "Ename" AS "Employee Name",
+	"Dno" AS "Department Number"
+FROM "employees"
+WHERE "Eno" = '900';
