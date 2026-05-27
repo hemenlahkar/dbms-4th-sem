c0c5694700a0645f2f17acb69cb0c7db5e9e41b0
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2026-05-28 01:52:11 +0530

    add query to see employees earning more than 2850$

---
+++ 05_salary.sql
@@ -0,0 +1,5 @@
+-- Query to display the Employee Name and Salary of all the employees earning more than $2850
+
+SELECT "Ename" AS "Employee Name", "Salary"
+FROM "employees"
+WHERE "Salary" > 2850;
